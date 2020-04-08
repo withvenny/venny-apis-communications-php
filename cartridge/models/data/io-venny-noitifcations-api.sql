aec289c180ffe98efc402283dbe1534b2a4d9658
adolphusnolan <adolphusnolan@gmail.com>
2020-04-07 21:07:20 -0500

    update to SQL for inital ID

--- cartridge/models/data/io-venny-noitifcations-api.sql
+++ cartridge/models/data/io-venny-noitifcations-api.sql
@@ -18,14 +18,14 @@ time_started	TIMESTAMPTZ	NOT NULL DEFAULT NOW(),
 time_updated	TIMESTAMPTZ	NOT NULL DEFAULT NOW(),
 time_finished	TIMESTAMPTZ	NOT NULL DEFAULT NOW(),
 active	INT	NOT NULL DEFAULT 1
-);		
+);
 CREATE SEQUENCE notifications_sequence;
 ALTER SEQUENCE notifications_sequence RESTART WITH 8301;
 ALTER TABLE notifications ALTER COLUMN ID SET DEFAULT nextval('notifications_sequence');
 -- ALTER TABLE notifications ADD FOREIGN KEY (profile_ID) REFERENCES profiles(profile_ID);
 -- ALTER TABLE notifications ADD FOREIGN KEY (app_ID) REFERENCES apps(app_ID);
-SELECT * FROM notifications;		
-DROP TABLE notifications;		
+SELECT * FROM notifications;
+DROP TABLE notifications;
 INSERT INTO notifications (notification_ID,notification_attributes,notification_message,notification_type,notification_opened,notification_viewed,notification_recipient,notification_sender,notification_subject,notification_object,profile_ID,app_ID,event_ID,process_ID)		
- VALUES ('30 characters','{}','lorem ipsum','30 characters','1','1','30 characters','30 characters','255 characters','255 characters','30 characters','30 characters','30 characters','30 characters');		
+ VALUES ('30-characters','{}','lorem ipsum','30 characters','1','1','30 characters','30 characters','255 characters','255 characters','30 characters','30 characters','30 characters','30 characters');		
 SELECT * FROM notifications;
